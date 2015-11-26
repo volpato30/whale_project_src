5c719e4a6171d5fe6228b27f2078632a17396386
睿 乔 <volpato30@gmail.com>
2015-11-25 21:31:35 -0500

    ..

--- data_process.py
+++ data_process.py
@@ -6,14 +6,14 @@ from PIL import Image
 import glob
 from collections import Counter
 
-os.chdir('./Fin/Resize')
+os.chdir('/scratch/rqiao/Fin/Resize')
 a=glob.glob('w*.jpg')
-os.chdir('/Users/ruiqiao/Documents/whale')
+os.chdir('..')
 train_label=pd.read_csv('train.csv')
 ids=train_label['whaleID']
 Iname=train_label['Image']
 idcount=Counter(ids).most_common()
-WNUM=20
+WNUM=200
 Wname=list()
 for tup in idcount[:WNUM]:
     Wname.append(tup[0])
@@ -24,7 +24,7 @@ for i in range(WNUM):
 
 target=[]
 data=np.zeros((1,3,256,256),dtype=np.float32)
-os.chdir('./Fin/Resize')
+os.chdir('./Resize')
 for i in range(WNUM):
     for file in Imgname[i]:
         temp=Image.open(file)
@@ -40,9 +40,9 @@ target=np.array(target,dtype=np.int16)
 length=len(target)
 arr=np.arange(length)
 np.random.shuffle(arr)
-train_i=arr[:int(np.floor(length*0.7))]
-valid_i=arr[int(np.floor(length*0.7)):int(np.floor(length*0.85))]
-test_i=arr[int(np.floor(length*0.85)):]
-np.savez("../resize_data2",data[train_i,:,:,:],target[train_i],data[valid_i,:,:,:],target[valid_i],data[test_i,:,:,:],target[test_i])
+train_i=arr[:int(np.floor(length*0.8))]
+valid_i=arr[int(np.floor(length*0.8)):int(np.floor(length*0.9))]
+test_i=arr[int(np.floor(length*0.9)):]
+np.savez("/scratch/rqiao/resize_dataF.npz",data[train_i,:,:,:],target[train_i],data[valid_i,:,:,:],target[valid_i],data[test_i,:,:,:],target[test_i])
 
 print("successful done")
