73c790d8319287455bec373a3b19e0ce4646777c
睿 乔 <volpato30@gmail.com>
2015-12-13 15:44:26 -0500

    requirement

---
+++ requirment.py
@@ -0,0 +1,14 @@
+
+from lasagne.layers import InputLayer
+from lasagne.layers import DenseLayer
+from lasagne.layers import ConcatLayer
+from lasagne.layers import NonlinearityLayer
+from lasagne.layers import GlobalPoolLayer
+from lasagne.layers.dnn import Conv2DDNNLayer as ConvLayer
+from lasagne.layers.dnn import MaxPool2DDNNLayer as PoolLayerDNN
+from lasagne.layers import MaxPool2DLayer as PoolLayer
+from lasagne.layers import LocalResponseNormalization2DLayer as LRNLayer
+from lasagne.nonlinearities import softmax, linear
+
+
+print('requirment fullfilled')
